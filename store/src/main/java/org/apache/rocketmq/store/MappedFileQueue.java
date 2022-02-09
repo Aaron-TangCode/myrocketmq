5abcc89c35cad243accd82588f801c966348fe6c
tanghailin <tanghailin@58.com>
2022-02-09 11:12:51 +0800

    消息发送存储流程

--- store/src/main/java/org/apache/rocketmq/store/MappedFileQueue.java
+++ store/src/main/java/org/apache/rocketmq/store/MappedFileQueue.java
@@ -197,6 +197,7 @@ public class MappedFileQueue {
         return 0;
     }
 
+    //创建MappedFile或者获取最后一个MappedFile
     public MappedFile getLastMappedFile(final long startOffset, boolean needCreate) {
         long createOffset = -1;
         MappedFile mappedFileLast = getLastMappedFile();
@@ -251,6 +252,7 @@ public class MappedFileQueue {
         return getLastMappedFile(startOffset, true);
     }
 
+    //取最后一个mappedfile文件
     public MappedFile getLastMappedFile() {
         MappedFile mappedFileLast = null;
 
